9fd54c14033c4bbb9b8e9fda0b28152a1c7ca4d2
nieyunliang <752000614@qq.com>
2023-09-11 21:29:17 +0800

    bugfix: 修复没有导入Alert包报错

--- src/pages/ChatAI/components/messages/messages.jsx
+++ src/pages/ChatAI/components/messages/messages.jsx
@@ -1,41 +1,41 @@
-import { Space, Image } from 'antd'
+import { Space, Image, Alert } from 'antd'
 import style from './index.module.less'
 import ChatGPTHeader from '@/assets/chatgpt-icon.svg'
 import MessageItem from './message-item.jsx'
 
 export default function Messages({ messages, loading, error }) {
-	return (
-		<div
-			id='messages-box'
-			className={style['messages-box']}
-		>
-			{messages.map(message => (
-				<MessageItem
-					key={message.id}
-					message={message}
-				/>
-			))}
+  return (
+    <div
+      id='messages-box'
+      className={style['messages-box']}
+    >
+      {messages.map(message => (
+        <MessageItem
+          key={message.id}
+          message={message}
+        />
+      ))}
 
-			{loading ? (
-				<div className={style['message-item']}>
-					<Space className={style.sender}>
-						<Image
-							width={30}
-							src={ChatGPTHeader}
-						/>
-					</Space>
-					<div className={style.bubble}>
-						<span className={style.cursor}>|</span>
-					</div>
-				</div>
-			) : null}
-			{error ? (
-				<Alert
-					message={error}
-					type='error'
-					showIcon
-				/>
-			) : null}
-		</div>
-	)
+      {loading ? (
+        <div className={style['message-item']}>
+          <Space className={style.sender}>
+            <Image
+              width={30}
+              src={ChatGPTHeader}
+            />
+          </Space>
+          <div className={style.bubble}>
+            <span className={style.cursor}>|</span>
+          </div>
+        </div>
+      ) : null}
+      {error ? (
+        <Alert
+          message={error}
+          type='error'
+          showIcon
+        />
+      ) : null}
+    </div>
+  )
 }
