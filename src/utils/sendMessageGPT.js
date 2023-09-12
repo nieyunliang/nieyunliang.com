75d1743cb5bd60828096821934524217cc6b82ed
nieyunliang <752000614@qq.com>
2023-09-12 22:35:52 +0800

    接口调整

--- src/utils/sendMessageGPT.js
+++ src/utils/sendMessageGPT.js
@@ -1,6 +1,6 @@
 export default function sendMessageGPT(messages) {
   return new Promise((resolve, reject) => {
-    fetch(`http://ai.nieyunliang.com/send_message`, {
+    fetch(`https://ai.nieyunliang.com/send_message`, {
       method: 'POST',
       headers: { 'Content-type': 'application/json' },
       body: JSON.stringify({
