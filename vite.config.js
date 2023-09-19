b4ae98c2c34de4763cb307a9220dfa6f3896f8f8
nieyunliang <752000614@qq.com>
2023-09-19 15:05:47 +0800

    refactor:  调整百度统计脚本，通过构建插入代码

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression'
 import path from 'path'
+import htmlPlugin from 'vite-plugin-html-config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression()],
   server: {
     port: 8000,
     proxy: {
@@ -17,5 +17,24 @@ export default defineConfig({
   },
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
-  }
+  },
+  plugins: [
+    react(),
+    viteCompression(),
+    htmlPlugin({
+      scripts: [
+        {
+          content: `
+						var _hmt = _hmt || [];
+            (function () {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?e136f7bf7934ff74190e817043cb4727";
+              var s = document.getElementsByTagName("script")[0];
+              s.parentNode.insertBefore(hm, s);
+            })();
+						`
+        }
+      ]
+    })
+  ]
 })
