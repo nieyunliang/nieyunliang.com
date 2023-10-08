43f3ac14da770c54fdf84f2f2d7e92496f6aeb68
nieyunliang <752000614@qq.com>
2023-10-08 16:56:56 +0800

    bugfix:  修复日期格式转换

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,26 +1,26 @@
 export const getFileUrl = path =>
-	new URL(`../assets/${path}`, import.meta.url).href
+  new URL(`../assets/${path}`, import.meta.url).href
 
 // 时间戳转日期
 export const formatDate = timestamp => {
-	if (Number.isInteger(timestamp)) {
-		const date = new Date(timestamp)
+  if (Number.isInteger(timestamp)) {
+    const date = new Date(timestamp)
 
-		const Y = date.getFullYear()
-		const M = `0${date.getMonth() + 1}`.slice(-2)
-		const D = date.getDate()
-		const h = date.getHours()
-		const m = `0${date.getMinutes()}`.slice(-2)
-		const s = `0${date.getSeconds()}`.slice(-2)
+    const Y = date.getFullYear()
+    const M = `0${date.getMonth() + 1}`.slice(-2)
+    const D = `0${date.getDate() + 1}`.slice(-2)
+    const h = date.getHours()
+    const m = `0${date.getMinutes()}`.slice(-2)
+    const s = `0${date.getSeconds()}`.slice(-2)
 
-		return `${Y}-${M}-${D} ${h}:${m}:${s}`
-	} else {
-		return timestamp
-	}
+    return `${Y}-${M}-${D} ${h}:${m}:${s}`
+  } else {
+    return timestamp
+  }
 }
 
 // 设备信息-iPhone或者Android
 export const isMobile = () => {
-	const { userAgent } = window.navigator
-	return !!~userAgent.indexOf('iPhone') || !!~userAgent.indexOf('Android')
+  const { userAgent } = window.navigator
+  return !!~userAgent.indexOf('iPhone') || !!~userAgent.indexOf('Android')
 }
